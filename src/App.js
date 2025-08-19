e92dff8549090371083105a92c4e57d957dc1644
Carolina Picetti <carolinapicetti@gmail.com>
2025-08-19 15:09:10 +0200

    added the complete footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ export default function App() {
   <footer>
     This project was coded by {" "} 
   <a href="https://github.com/CarolPicetti">Carolina Picetti</a> and is on {" "}
-  <a href="https://github.com/CarolPicetti/weather-react" target="_blank" rel="noreferrer">Github</a>
+  <a href="https://github.com/CarolPicetti/weather-react" target="_blank" rel="noreferrer">Github {" "}</a>
+  and hosted on {" "}
+  <a href="https://app.netlify.com/teams/carolpicetti/projects" target="_blank" rel="noreferrer">Netlify</a>
   </footer>
   </div>
   </div>
